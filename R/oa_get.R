629a4f1ee59c00337f1696e6ab4b9fef4e3a0414
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-25 12:47:12 -0700

    fixed stuff :trolleybus: :ant: :turtle:

--- R/oa_get.R
+++ R/oa_get.R
@@ -1,65 +1,41 @@
 #' Get data
 #'
 #' @export
-#' @import dplyr httr
+#' @import dplyr httr readr
 #'
-#' @param x Anything coercable to an object of class erddap_info. So the output of a call to
-#' \code{erddap_info}, or a datasetid, which will internally be passed through \code{erddap_info}.
-#' @param ... Dimension arguments.
-#' @param fields Fields to return, a character vector.
-#' @param stride (integer) How many values to get. 1 = get every value, 2 = get every other value,
-#' etc. Default: 1 (i.e., get every value)
-#' @param fmt (character) One of csv (default) or ncdf
-#' @param store One of \code{disk} (default) or \code{memory}. You can pass options to \code{disk}
+#' @param x State name
+#' @param path Path to store files in, a directory, not the file name
 #' @param ... Pass on curl options to \code{\link[httr]{GET}}
 #'
-#' @references \url{http://upwell.pfeg.noaa.gov/erddap/index.html}
+#' @references \url{http://openaddresses.io/}
 #' @examples \dontrun{
-#' oa_get()
+#' res <- oa_get(x = "20150316/us-nc-burke.csv")
 #' }
 
-oa_get <- function(x, ...) {
-  if(all(var == "none")){
-    args <- paste0(sapply(dims, function(y) parse_args(x, y, stride, dimargs, wname = TRUE)), collapse = ",")
-  } else {
-    pargs <- sapply(dims, function(y) parse_args(x, y, stride, dimargs))
-    args <- paste0(lapply(var, function(y) paste0(y, paste0(pargs, collapse = ""))), collapse = ",")
-  }
-  resp <- erd_up_GET(url = sprintf("%sgriddap/%s.%s", eurl(), d, fmt), dset = d, args, store, fmt, callopts)
-  loc <- if(store$store == "disk") resp else "memory"
-  structure(read_upwell(resp, fmt), class=c("erddap_grid","data.frame"), datasetid=d, path=loc)
+oa_get <- function(x, path = "~/.openadds", ...) {
+  resp <- oa_GET(url = sprintf("http://data.openaddresses.io.s3.amazonaws.com/%s", x), x, path, ...)
+  # loc <- if (store$store == "disk") resp else "memory"
+  structure(list(data = resp), class = c("oa", "data.frame"), id = x, path = make_path(x, path))
+}
+
+# "http://data.openaddresses.io.s3.amazonaws.com/20150316/us-nc-burke.csv"
+# "http://data.openaddresses.io.s3.amazonaws.com/20150422/us-or-oregon_city.csv"
+# url <- "http://s3.amazonaws.com/openaddresses/20140822/us-nc-washington.zip"
+# GET(url, write_disk("wash.zip"))
+
+oa_GET <- function(url, x, path, ...){
+  file <- make_path(x, path)
+  res <- GET(url, write_disk(file, TRUE))
+  stop_for_status(res)
+  readr::read_csv(res$request$writer[[1]])
+}
+
+make_path <- function(x, path) {
+  file.path(path, strsplit(x, "/")[[1]][[2]])
 }
 
 #' @export
-print.erddap_grid <- function(x, ..., n = 10){
-  cat(sprintf("<NOAA ERDDAP griddap> %s", attr(x, "datasetid")), sep = "\n")
-  cat(sprintf("   Path: [%s]", attr(x, "path")), sep = "\n")
-  if(attr(x, "path") != "memory"){
-    finfo <- file_info(attr(x, "path"))
-    cat(sprintf("   Last updated: [%s]", finfo$mtime), sep = "\n")
-    cat(sprintf("   File size:    [%s]", finfo$size), sep = "\n")
-  }
-  cat(sprintf("   Dimensions:   [%s X %s]\n", NROW(x), NCOL(x)), sep = "\n")
-  trunc_mat_(x, n = n)
-}
-
-oa_GET <- function(url, dset, args, store, fmt, ...){
-  if(store$store == "disk"){
-    out <- suppressWarnings(cache_get(cache = TRUE, url = url, args = args, path = store$path))
-
-    # fpath <- path.expand(file.path(store$path, paste0(dset, ".csv")))
-    if( !is.null( out ) && !store$overwrite ){
-      out
-    } else {
-      dir.create(store$path, showWarnings = FALSE, recursive = TRUE)
-      res <- GET(url, query=args, write_disk(write_path(store$path, url, args, fmt), store$overwrite), ...)
-      out <- check_response_erddap(res, fmt)
-      if(res$status_code != 200) unlink(res$request$writer[[1]])
-      if(grepl("Error", out, ignore.case = TRUE)) NA else res$request$writer[[1]]
-    }
-  } else {
-    res <- GET(url, query=args, ...)
-    out <- check_response_erddap(res, fmt)
-    if(grepl("Error", out, ignore.case = TRUE)) NA else res
-  }
+print.oa <- function(x, ..., n = 10){
+  cat(sprintf("<Openaddresses data> %s", attr(x, "path")), sep = "\n")
+  print(x$data, n = n)
 }
